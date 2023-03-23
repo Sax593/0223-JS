c215775726276fc959df8767d2c41fcc1c267b7a
sax59 <waquetv@gmail.com>
2023-03-23 15:20:18 +0100

    Add HTML/Css/JS exercice

---
+++ Prepa cp1/html/index.js
@@ -0,0 +1,9 @@
+const modalOne = document.getElementById("art1-mod");
+const modalTwo = document.getElementById("art2-mod");
+const modalThree = document.getElementById("art3-mod");
+
+const btnOne = document.getElementById("infoOne");
+const btnTwo = document.getElementById("infoTwo");
+const btnThree = document.getElementById("infoThree");
+
+// Ajout des animations pour afficher les modals.
