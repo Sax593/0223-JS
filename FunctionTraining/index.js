d94d2242db10a90387d1e60e50b1a801f2e7cbfc
sax59 <waquetv@gmail.com>
2023-03-29 16:53:12 +0200

    Add Function training in JS

---
+++ FunctionTraining/index.js
@@ -0,0 +1,16 @@
+//Consigne général, avant de coder votre fonction faite un peu de no code avant.
+
+//EXERCICE 1
+// créer une fonction qui calcul le nombre de jours passer.
+// exemple avec 21 : "Vous avez vécu 7665 jours."
+
+//EXERCICE 2
+// demander un nombre entre 0 et 5 à l'utilisateur
+// si l'utilisateur n'entre pas un nombre entre 0 et 5, le signaler par une alert
+
+//EXERCICE 3
+// définir un nombre à deviner (et pourquoi pas avec Math.random()?)
+// récupérer un nombre entré par l'utilisateur
+// si le nombre de l'utilisateur est plus grand que celui à deviner, le dire
+// si le nombre de l'utilisateur est plus petit que celui à deviner, le dire
+// si le nombre est identique au NOMBRECIBLE, afficher "Gagné!"
