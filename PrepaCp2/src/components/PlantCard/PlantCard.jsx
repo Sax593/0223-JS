d18eab323e4a8a53a3ba95caa3cce035d70a289e
sax59 <waquetv@gmail.com>
2023-05-04 15:57:53 +0200

    Add instruction in Cp2 trainig

--- PrepaCp2/src/components/PlantCard/PlantCard.jsx
+++ PrepaCp2/src/components/PlantCard/PlantCard.jsx
@@ -1,5 +1,7 @@
 export default function PlantCard() {
+  // Step 1 : Import Props in you components
   return (
+    // Step 2 : Use Props on you return
     <>
       <h1>Plant name</h1>
       <h2>Scientifique Name</h2>
