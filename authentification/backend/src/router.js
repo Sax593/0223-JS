3d53165b9df8dbdfb95010602a11120860daeb38
sax59 <waquetv@gmail.com>
2023-06-20 14:47:59 +0200

    Add authentification LC

---
+++ authentification/backend/src/router.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const router = express.Router();
+
+const itemControllers = require("./controllers/itemControllers");
+
+router.get("/items", itemControllers.browse);
+router.get("/items/:id", itemControllers.read);
+router.put("/items/:id", itemControllers.edit);
+router.post("/items", itemControllers.add);
+router.delete("/items/:id", itemControllers.destroy);
+
+const loginControllers = require("./controllers/loginControllers");
+
+router.post("/login/users", loginControllers.login);
+
+module.exports = router;
