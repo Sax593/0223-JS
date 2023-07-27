da372971accb54f3870f6f4f579ec14fb87a57c5
sax59 <waquetv@gmail.com>
2023-07-27 13:33:54 +0200

    add test

---
+++ testLC/end2endTest/tests/github.spec.js
@@ -0,0 +1,11 @@
+const { test, expect } = require("@playwright/test");
+
+test("Able to login with valid credentials", async ({ page }) => {
+  await page.goto("https://github.com/login");
+  await page.getByPlaceholder("your@email.com").click();
+  await page.getByPlaceholder("your@email.com").fill("test@test.com");
+  await page.getByLabel("Your password").click();
+  await page.getByLabel("Your password").fill("1234");
+  await page.getByRole("button", { name: "Sign in" }).click();
+  await page.pause();
+});
--- testLC/integrationtest/src/App.test.tsx
+++ testLC/integrationtest/src/App.test.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import Cart from "./Card";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+describe("Cart", () => {
+  it("renders a button", () => {
+    render(<Cart />);
+
+    expect(screen.getByRole("button")).toBeInTheDocument();
+  });
+
+  describe("When user clicks purchase button", () => {
+    it("renders Thank you!", () => {
+      render(<Cart />);
+
+      fireEvent.click(screen.getByRole("button"));
+
+      expect(screen.getByText(/Thank you!/i)).toBeInTheDocument();
+    });
+  });
 });
